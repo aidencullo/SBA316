9dcd8c65b6e4dc3d1a980ae12a38c895f636c0e5
Aiden Cullo <aidencullo@gmail.com>
2024-08-18 16:27:53 -0400

    Cache at least one element using selectElementById.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const registration = document.getElementById("registration");
+const registration = document.getElementById("lottery-registration");
 
 function showError(element, message, evt) {
   alert(message);
@@ -7,32 +7,12 @@ function showError(element, message, evt) {
 }
 
 function validateRegistration(evt) {
-  const { username, email, password, passwordCheck } = registration.elements;
+  const { fullName } = registration.elements;
   const errors = [];
 
-  // Validate Username
-  const usernameVal = username.value;
-  const uniqueChars = new Set(usernameVal);
-  if (uniqueChars.size < 2) {
-    errors.push({ element: username, message: "Your username must contain at least two unique characters." });
-  } else if (localStorage.getItem(usernameVal.toLowerCase())) {
-    errors.push({ element: username, message: "Your username is already taken." });
-  }
-
-  // Validate Email
-  const emailVal = email.value;
-  if (emailVal.includes("example.com")) {
-    errors.push({ element: email, message: "Your email address must not contain 'example.com'." });
-  }
-
-  // Validate Password
-  const passwordVal = password.value;
-  if (passwordVal.toLowerCase().includes("password")) {
-    errors.push({ element: password, message: "Your password must not contain the word 'password'." });
-  } else if (passwordVal.includes(usernameVal)) {
-    errors.push({ element: password, message: "Your password must not contain your username." });
-  } else if (passwordVal !== passwordCheck.value) {
-    errors.push({ element: password, message: "Your passwords do not match." });
+  const fullNameVal = fullName.value.trim();
+  if (fullNameVal === "aiden") {
+    errors.push({ element: fullName, message: "Your username cannot be 'aiden'." });
   }
 
   if (errors.length > 0) {
@@ -40,51 +20,9 @@ function validateRegistration(evt) {
     showError(errors[0].element, errors[0].message, evt);
     return false;
   }
-
-  // Store Credentials
-  localStorage.setItem(usernameVal.toLowerCase(), JSON.stringify({
-    email: emailVal.toLowerCase(),
-    password: passwordVal
-  }));
 
   alert("Your registration was successful!");
   return true;
 }
 
 registration.addEventListener("submit", validateRegistration);
-
-const login = document.getElementById("login");
-
-
-function validateLogin(evt) {
-  const { username, password, persist } = login.elements;
-  const errors = [];
-
-  // Validate Username
-  const usernameVal = username.value;
-  const user = JSON.parse(localStorage.getItem(usernameVal.toLowerCase()));
-  if (!user) {
-    errors.push({ element: username, message: "Your username does not exist." });
-  }
-
-  // Validate Password
-  const passwordVal = password.value;
-  if (user?.password !== passwordVal) {
-    errors.push({ element: username, message: "Your password is incorrect." });
-  }
-
-  if (errors.length > 0) {
-    // Show the first error and stop form submission
-    showError(errors[0].element, errors[0].message, evt);
-    return false;
-  }
-
-  if (persist.checked) {
-    alert("Your login was successful! Your credentials will be stored.");
-  } else {
-    alert("Your login was successful!");
-  }
-  return true;
-}
-
-login.addEventListener("submit", validateLogin);
